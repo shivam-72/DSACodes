241eb677fb8343c99083dcaf32a1c83908ccc75d
Shivam Prakash <85667261+shivam-72@users.noreply.github.com>
2025-03-27 17:27:24 +0530

    Time: 0 ms (100%), Space: 89.9 MB (85.66%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int count=0;
+        int res=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==res){
+               count++;
+            }
+           else if(count==0){
+               res=nums[i];
+                count=1;
+            }
+           
+            else
+                count--;
+        }
+        count=0;
+        for(auto x:nums){
+            if(x==res)
+            count++;
+        }
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]==res){
+            c++;
+           }
+           if(c*2>(i+1)&&(count-c)*2>(nums.size()-i-1)){
+            return i;
+           }
+        }
+        return -1;
+    }
+};
